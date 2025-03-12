4dac27e3d9de2658d1434966e6bea6f41ef3b37b
Kumar Anurag <me@kmranrg.com>
2025-03-12 21:21:57 +0000

    flightgoggles running in ubuntu

--- pyFlightGoggles_/build/temp.linux-x86_64-3.8/libs/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/pyFlightGoggles/libs/yaml-cpp
-# Build directory: /root/pyFlightGoggles/build/temp.linux-x86_64-3.8/libs/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
--- pyFlightGoggles_/build/temp.linux-x86_64-3.8/libs/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/pyFlightGoggles/libs/yaml-cpp/util
-# Build directory: /root/pyFlightGoggles/build/temp.linux-x86_64-3.8/libs/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
